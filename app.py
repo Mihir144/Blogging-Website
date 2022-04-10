bb474e0820a7815942d052c7c801a8bbf91da64b
Mihir <19it068@charusat.edu.in>
2022-04-10 15:35:03 +0530

    added form validation, registration and login page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY']="MIHIRMODI"
 
 post=[
     {
@@ -23,10 +24,33 @@ post=[
     }
 ]
 
+
 @app.route('/')
-@app.route('/Home')
-def hello_world():
+@app.route('/home')
+def HomePage():
     return render_template('Home.html',posts=post,title="Home Page")
+
+@app.route('/about')
+def About():
+    return render_template('About.html',title="About Page")
+
+
+@app.route('/login',methods=['GET','POST'])
+def Login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Logged In Successfully')
+        return redirect(url_for("HomePage"))
+    return render_template('Login.html',title="Login",form=form)
+
+
+@app.route('/register',methods=['GET','POST'])
+def Register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!')
+        return redirect(url_for("HomePage"))
+    return render_template('Register.html',title="Registration",form=form)
 
 
 if __name__ == '__main__':
