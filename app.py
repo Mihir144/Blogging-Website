ff08aed429a83f2288ee6dbdbe738cb682a54e14
Mihir <19it068@charusat.edu.in>
2022-04-10 13:43:35 +0530

    added templates

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+post=[
+    {
+        'author':'Mihir Modi',
+        'title':'Blog Post 1',
+        'content':'first post content',
+        'date_posted':'april 20,2018'
+    },
+    {
+        'author':'Ruchit Modi',
+        'title':'Blog Post 1',
+        'content':'first post content',
+        'date_posted':'april 20,2018'
+    },
+    {
+        'author':'Ayush Modi',
+        'title':'Blog Post 1',
+        'content':'first post content',
+        'date_posted':'april 20,2018'
+    }
+]
 
 @app.route('/')
+@app.route('/Home')
 def hello_world():
-    return 'Hello World!'
+    return render_template('Home.html',posts=post,title="Home Page")
 
 
 if __name__ == '__main__':
